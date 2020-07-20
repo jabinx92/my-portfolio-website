7fb141dfbd07e0fe37b6d211f45d6a5838486608
Jonathan Won <jabinx92@gmail.com>
2020-07-20 16:01:39 -0700

    Revert "Revert "change favicon + background color""
    This reverts commit e170a6072236928922388b5eb8ce74906c81ccbc.

--- src/styles/theme.js
+++ src/styles/theme.js
@@ -1,14 +1,18 @@
 import { hex2rgba } from '@utils';
 
-const ACCENT = '#64ffda';
-const DARK_BG = '#020c1b';
-const BG = '#0a192f';
+
+//https://colorswall.com/palette/868/ - matrix color theme
+
+const ACCENT = '#22b455';
+const DARK_BG = '#020204';
+const BG = '#020204';
+
 
 const theme = {
   colors: {
     darkNavy: DARK_BG,
     navy: BG,
-    lightNavy: '#172a45',
+    lightNavy: '#181818',
     lightestNavy: '#303C55',
     slate: '#8892b0',
     lightSlate: '#a8b2d1',
